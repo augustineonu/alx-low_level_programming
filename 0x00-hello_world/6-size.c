c701cc0837ef1917f519d34d4da8aad9bbb47739
augustineonu <onuaugustine07@gmail.com>
2022-03-10 13:43:07 -0800

    push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -17,5 +17,5 @@ int main(void)
         printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
  	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
-	return(0);
+	return (0);
 }
