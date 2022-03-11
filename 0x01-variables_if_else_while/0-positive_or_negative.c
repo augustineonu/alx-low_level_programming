4d28c99e5f949478f7a4b41044e123d14cc72f1c
augustineonu <onuaugustine07@gmail.com>
2022-03-11 07:44:00 -0800

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/*
+/**
  * main - completing the source code in orde to print where the number is negative or positive
  * Return: 0
  */
