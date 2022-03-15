5754d50b15a55b8ebe6c7fb1ae3faa28d5894526
augustineonu <onuaugustine07@gmail.com>
2022-03-15 10:10:44 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  * _putchar print the characters given
  * Return: Always 0 (Success)
  */
- 
+
 int main(void)
 {
 	_putchar('_');
@@ -14,9 +14,9 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
+	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
-
