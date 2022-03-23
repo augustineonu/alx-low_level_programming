bd002f0a53dad10432c6e132fd7eb576079f3531
augustineonu <onuaugustine07@gmail.com>
2022-03-23 16:32:03 -0700

    arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,10 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int a, b;
 	a = 0;
-	while(dest[a] != '\0')
+
+	while (dest[a] != '\0')
 		a++;
 	b = 0;
-	while(src[b] != '\0' && b < n)
+	while (src[b] != '\0' && b < n)
 	{
 		dest[a] = src[b];
 		a++;
