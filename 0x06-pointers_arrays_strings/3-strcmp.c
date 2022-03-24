ee8cdfde03b0cfa8495b9f29efce6d9108913371
augustineonu <onuaugustine07@gmail.com>
2022-03-23 17:52:38 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 
 {
 	int i = 0;
-	
+
 	int t;
 
 	while (s1[i] != '\0' || s2[i] != '\0')
@@ -21,7 +21,6 @@ int _strcmp(char *s1, char *s2)
 		{
 			t = (s1[i] - s2[i]);
 			break;
-
 		}
 		else
 		{
