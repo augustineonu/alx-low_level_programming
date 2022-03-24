042c7dda004e071a9e70c74e48e99e7e277a6def
augustineonu <onuaugustine07@gmail.com>
2022-03-24 13:53:56 -0700

    lookup

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes the letters of the string to uppercase
+ * @c: string pointer
+ * Return: c
+ */
+
+char *string_toupper(char *c)
+{
+	int a;
+
+	for (a = 0; c[a] != '\0'; a++)
+	{
+		if (c[a] > 96 && c[a] < 123)
+		{
+			c[a] -= 32;
+		}
+	}
+	return (c); 
+}
