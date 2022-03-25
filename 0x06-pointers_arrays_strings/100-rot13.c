28e3a47dabd6a4e99bf55144b861c38e8703b74b
augustineonu <onuaugustine07@gmail.com>
2022-03-24 18:49:32 -0700

    encoding s string

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - a function that replaces the next 13th character
- *
+ * rot13 - a function that encodes a string
+ * @s: characters in an array
+ * Return: s
  */
 
 char *rot13(char *s)
 {
-        int i, j;
+	int i, j;
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-        char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                for (j = 0; j < 52; j++)
-                {
-                        if (s[i] == a[j])
-                        {
-                                s[i] = b[j];
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
 				break;
-                        }
-                }
-        }
-        return (s);
+			}
+		}
+	}
+	return (s);
 }
-
