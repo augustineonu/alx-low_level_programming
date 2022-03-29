94964ed84d3ae822e1860d63671bd83e742920d2
augustineonu <onuaugustine07@gmail.com>
2022-03-28 17:30:12 -0700

    upstring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, count = 0;
 	
-	for (i = 0; s[i] != '\0' && i == count; i++;)
+	for (i = 0; s[i] != '\0' && i == count; i++)
 		for (j = 0; accept[j] != '\0'; j++)
 			if (s[i] == accept[j])
 				count++;
